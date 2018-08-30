475c5264ac3b8b052f207f4ae12235ee07671b84
gokul3642 <gokul3642@gmail.com>
2018-08-30 21:35:18 +0530

    Add files via upload

---
+++ SalesClassLibrary/ISaleService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace SalesClassLibrary
+{    
+    [ServiceContract]
+    interface ISaleService
+    {
+        [OperationContract]
+        bool InsertCustomer(Customer obj);
+
+        [OperationContract]
+        List<Customer> GetAllCustomer();
+
+        [OperationContract]
+        bool DeleteCustomer(int Cid);
+
+        [OperationContract]
+        bool UpdateCustomer(Customer obj);  
+    }
+    [DataContract]
+    public class Customer
+    {
+        [DataMember]
+        public int CustomerID;
+        [DataMember]
+        public string CustomerName;
+        [DataMember]
+        public string Address;
+        [DataMember]
+        public string EmailId;
+    } 
+}
